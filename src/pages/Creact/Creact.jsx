2573454ee2b77e9d62e3103e0093ad37caad26f9
mohammadjalizi <mjalizi1376@gmail.com>
2023-03-25 14:16:17 +0330

    added project

--- src/pages/Creact/Creact.jsx
+++ src/pages/Creact/Creact.jsx
@@ -2,14 +2,26 @@ import React from 'react'
 import TextField from '@mui/material/TextField';
 import InputAdornment from '@mui/material/InputAdornment';
 import { Box } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import { purple } from '@mui/material/colors';
+import { ChevronRight } from '@mui/icons-material';
+const ColorButton = styled(Button)(({ theme }) => ({
+  color: theme.palette.getContrastText(purple[500]),
+  backgroundColor: purple[500],
+  '&:hover': {
+    backgroundColor: purple[700],
+  },
+}));
 const Creact = () => {
   return (
-    <Box sx={{width:'366px'}} component="form">   
+    <Box sx={{width:'366px'}} component="form" >   
     <TextField
-  
+
     label="transtitile"
    
-    sx={{   width:'full', m: 1, mt: '22px' }}
+    sx={{    m: 1, mt: '22px' }}
     InputProps={{
       startAdornment: <InputAdornment position="start">👉</InputAdornment>,
     }}
@@ -19,13 +31,17 @@ const Creact = () => {
   <TextField
     label="transtitile"
 
-    sx={{ m: 1, mt: '22px' }}
+    sx={{ m: 1, mt: '22px', }}
     InputProps={{
-      startAdornment: <InputAdornment position="start">kg</InputAdornment>,
+      startAdornment: <InputAdornment position="start">$</InputAdornment>,
     }}
     variant="filled"
   />
-
+  <br/>
+ <ColorButton sx={{mt:'22px'}} variant="contained">
+ submit <ChevronRight/>
+ 
+ </ColorButton>
 
     </Box>
   )
