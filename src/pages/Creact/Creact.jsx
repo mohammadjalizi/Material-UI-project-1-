5bc428b40779a74a29a62815a0bf55a7c19567bd
mohammadjalizi <mjalizi1376@gmail.com>
2023-03-25 13:44:34 +0330

    added project

--- src/pages/Creact/Creact.jsx
+++ src/pages/Creact/Creact.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
-
+import TextField from '@mui/material/TextField';
+import InputAdornment from '@mui/material/InputAdornment';
+import { Box } from '@mui/material';
 const Creact = () => {
   return (
-    <div>Creact</div>
+    <Box sx={{width:'366px'}} component="form">   
+    <TextField
+  
+    label="transtitile"
+   
+    sx={{   width:'full', m: 1, mt: '22px' }}
+    InputProps={{
+      startAdornment: <InputAdornment position="start">👉</InputAdornment>,
+    }}
+    variant="filled"
+  />
+  <br/>
+  <TextField
+    label="transtitile"
+
+    sx={{ m: 1, mt: '22px' }}
+    InputProps={{
+      startAdornment: <InputAdornment position="start">kg</InputAdornment>,
+    }}
+    variant="filled"
+  />
+
+
+    </Box>
   )
 }
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,23 @@
+import { Close } from '@mui/icons-material'
+import { Box, Paper, Typography, IconButton } from '@mui/material'
 import React from 'react'
 
 const Home = () => {
   return (
-    <div>Homeeeeeeeeeeeee</div>
+   <Box>
+
+<Paper sx={{width:"366px",display:'flex',justifyContent:'space-between',mt:'22px',pt:'27px',position:'relative'}}>
+
+<Typography variant="h6" > GYM</Typography>
+<Typography variant="h6" sx={{mr:'33px',fontWeight:500,opacity:'0.7'}} > $100</Typography>
+<IconButton sx={{position:'absolute',top:'0',right:'0'}}  >
+  <Close sx={{fontSize:'22px'}}/>
+</IconButton>
+
+</Paper>
+
+
+   </Box>
   )
 }
 
