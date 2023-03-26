15680093fa6a567898d57caa58a5d1f9d5660de5
mohammadjalizi <mjalizi1376@gmail.com>
2023-03-26 14:13:51 +0330

    added project

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {
 } from "react-router-dom";
 import Creact from "./pages/Creact/Creact";
 import Home from "./pages/Home/Home";
+import NotFound from "./pages/NotFound";
 import Root from "./pages/Root";
 
 const router = createBrowserRouter(
@@ -13,7 +14,7 @@ const router = createBrowserRouter(
     <Route path="/" element={<Root />}>
       <Route index element={<Home />} />
       <Route path="create" element={<Creact />} />
-
+      <Route path="*" element={<NotFound />} />
       {/* ... etc. */}
     </Route>
   )
