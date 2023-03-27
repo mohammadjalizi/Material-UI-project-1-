6f5df185390ec449f409c71f6f0a629522abe052
mohammadjalizi <mjalizi1376@gmail.com>
2023-03-27 16:17:43 +0330

    added project

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -11,18 +11,25 @@ const Home = () => {
     .then((data) => setmydata(data) )
 
   },[])
+  console.log(mydata)
   return (
    <Box>
+{mydata.map(item=>{
+return(
 
-<Paper sx={{width:"366px",display:'flex',justifyContent:'space-between',mt:'22px',pt:'27px',position:'relative'}}>
+  <Paper sx={{width:"366px",display:'flex',justifyContent:'space-between',mt:'22px',pt:'27px',position:'relative'}}>
 
-<Typography variant="h6" > GYM</Typography>
-<Typography variant="h6" sx={{mr:'33px',fontWeight:500,opacity:'0.7'}} > $100</Typography>
+<Typography variant="h6" > {item.name}</Typography>
+<Typography variant="h6" sx={{mr:'33px',fontWeight:500,opacity:'0.7'}} > ${item.price}</Typography>
 <IconButton sx={{position:'absolute',top:'0',right:'0'}}  >
   <Close sx={{fontSize:'22px'}}/>
 </IconButton>
 
 </Paper>
+)
+
+})}
+
 
 
    </Box>
