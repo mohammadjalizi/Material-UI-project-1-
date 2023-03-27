6d1b0e8067a7306cf25cc3e7a78314b631454026
mohammadjalizi <mjalizi1376@gmail.com>
2023-03-27 16:19:16 +0330

    added project

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -19,7 +19,7 @@ return(
 
   <Paper sx={{width:"366px",display:'flex',justifyContent:'space-between',mt:'22px',pt:'27px',position:'relative'}}>
 
-<Typography variant="h6" > {item.name}</Typography>
+<Typography variant="h6" > {item.title}</Typography>
 <Typography variant="h6" sx={{mr:'33px',fontWeight:500,opacity:'0.7'}} > ${item.price}</Typography>
 <IconButton sx={{position:'absolute',top:'0',right:'0'}}  >
   <Close sx={{fontSize:'22px'}}/>
