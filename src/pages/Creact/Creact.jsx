3cecdd9015aa7f883c406594f8b6701852cef40d
mohammadjalizi <mjalizi1376@gmail.com>
2023-03-27 16:00:34 +0330

    added project

--- src/pages/Creact/Creact.jsx
+++ src/pages/Creact/Creact.jsx
@@ -1,70 +1,68 @@
-import React, { useState } from 'react'
-import TextField from '@mui/material/TextField';
-import InputAdornment from '@mui/material/InputAdornment';
-import { Box } from '@mui/material';
-import { styled } from '@mui/material/styles';
-import Button from '@mui/material/Button';
-import Stack from '@mui/material/Stack';
-import { purple } from '@mui/material/colors';
-import { ChevronRight } from '@mui/icons-material';
+import React, { useState } from "react";
+import TextField from "@mui/material/TextField";
+import InputAdornment from "@mui/material/InputAdornment";
+import { Box } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+
+import { purple } from "@mui/material/colors";
+import { ChevronRight } from "@mui/icons-material";
 const ColorButton = styled(Button)(({ theme }) => ({
   color: theme.palette.getContrastText(purple[500]),
-  backgroundColor:theme.palette.ali.main,
-  '&:hover': {
+  backgroundColor: theme.palette.ali.main,
+  "&:hover": {
     backgroundColor: purple[700],
   },
 }));
 const Creact = () => {
-  const[title,settilte]=useState("")
-  const[price,setprice]=useState(0)
+  const [title, settilte] = useState("");
+  const [price, setprice] = useState(0);
   return (
-    <Box sx={{width:'366px'}} component="form" >   
-    <TextField
-      onChange={(eo)=>{
-        settilte(eo.target.value)
-        
-          }}
-fullWidth={true}
-    label="transtitile"
-   
-    sx={{    m: 1, mt: '22px' }}
-    InputProps={{
-      startAdornment: <InputAdornment position="start">👉</InputAdornment>,
-    }}
-    variant="filled"
-  />
-  <br/>
-  <TextField
-onChange={(eo)=>{
-setprice(Number(eo.target.value))
-
-}}
-  fullWidth={true}  
-    label="transtitile"
-
-    sx={{ m: 1, mt: '22px',  }}
-    InputProps={{
-      startAdornment: <InputAdornment position="start">$</InputAdornment>,
-    }}
-    variant="filled"
-  />
-  <br/>
- <ColorButton  onClick={()=>{
-fetch("  http://localhost:3002/mydata",{
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify(data),
-})
-
- }}  sx={{mt:'22px'}} variant="contained">
- submit <ChevronRight/>
- 
- </ColorButton>
-
+    <Box sx={{ width: "366px" }} component="form">
+      <TextField
+        onChange={(eo) => {
+          settilte(eo.target.value);
+          console.log(setprice)
+        }}
+        fullWidth={true}
+        label="transtitile"
+        sx={{ m: 1, mt: "22px" }}
+        InputProps={{
+          startAdornment: <InputAdornment position="start">👉</InputAdornment>,
+        }}
+        variant="filled"
+      />
+      <br />
+      <TextField
+        onChange={(eo) => {
+          setprice(Number(eo.target.value));
+        }}
+        fullWidth={true}
+        label="transtitile"
+        sx={{ m: 1, mt: "22px" }}
+        InputProps={{
+          startAdornment: <InputAdornment position="start">$</InputAdornment>,
+        }}
+        variant="filled"
+      />
+      <br />
+      <ColorButton
+        onClick={() => {
+          fetch("http://localhost:3100/mydata", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({price, title}),
+          });
+        }}
+        sx={{ mt: "22px" }}
+        variant="contained"
+      >
+        submit <ChevronRight />
+      </ColorButton>
     </Box>
-  )
-}
+  );
+};
 
-export default Creact
+export default Creact;
