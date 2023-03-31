51445ef89ff7ec9c4dff3d9b7ae5fbbe8358a34c
mohammadjalizi <mjalizi1376@gmail.com>
2023-03-31 16:34:27 +0330

    form  Hook Form

--- src/pages/Creact/Creact.jsx
+++ src/pages/Creact/Creact.jsx
@@ -1,78 +1,97 @@
-import React, { useState } from "react";
-import TextField from "@mui/material/TextField";
-import InputAdornment from "@mui/material/InputAdornment";
-import { Box } from "@mui/material";
-import { styled } from "@mui/material/styles";
-import Button from "@mui/material/Button";
+import { Box, Button, InputAdornment, TextField, styled } from "@mui/material";
 
+import React from "react";
 import { purple } from "@mui/material/colors";
 import { ChevronRight } from "@mui/icons-material";
-import {  useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+
 const ColorButton = styled(Button)(({ theme }) => ({
   color: theme.palette.getContrastText(purple[500]),
+  // @ts-ignore
   backgroundColor: theme.palette.ali.main,
   "&:hover": {
-    backgroundColor: purple[700],
+    // @ts-ignore
+    backgroundColor: theme.palette.ali.main,
+    scale: "0.99",
   },
 }));
-const Creact = () => {
-  const [title, settilte] = useState("");
-  const [price, setprice] = useState(0);
-   const navigate=useNavigate()
-   
+
+const Create = () => {
+  const navigate = useNavigate();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = ({ price, title }) => {
+    price = Number(price);
+    fetch("http://localhost:3100/mydata", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ price, title }),
+    }).then(() => {
+      navigate("/");
+    });
+  };
+
   return (
-    <Box sx={{ width: "366px" }} component="form">
+    <Box
+      onSubmit={handleSubmit(onSubmit)}
+      autoComplete="off"
+      sx={{ width: "380px" }}
+      component="form"
+    >
       <TextField
-           autoComplete="off"
-        onChange={(eo) => {
-          settilte(eo.target.value);
-          console.log(setprice)
-        }}
         fullWidth={true}
-        label="transtitile"
-        sx={{ m: 1, mt: "22px" }}
+        label="Transaction Title"
+        sx={{ mt: "22px", display: "block" }}
         InputProps={{
           startAdornment: <InputAdornment position="start">👉</InputAdornment>,
         }}
         variant="filled"
+        {...register("title", {
+          required: { value: true, message: "Requires field" },
+          minLength: { value: 3, message: "minumn length is 3" },
+        })}
+        error={Boolean(errors.title)}
+        helperText={
+          Boolean(errors.title) ? errors.title?.message.toString() : null
+        }
       />
-      <br />
+
       <TextField
-          autoComplete="off"
-        onChange={(eo) => {
-          setprice(Number(eo.target.value));
-        }}
+        error={Boolean(errors.price)}
+        helperText={
+          Boolean(errors.price) ? errors.price?.message.toString() : null
+        }
+        {...register("price", {
+          required: { value: true, message: "Required filed" },
+        })}
         fullWidth={true}
-        label="transtitile"
-        sx={{ m: 1, mt: "22px" }}
+        label=" Amount"
+        id="filled-start-adornment"
+        sx={{ mt: "22px", display: "block" }}
         InputProps={{
           startAdornment: <InputAdornment position="start">$</InputAdornment>,
         }}
         variant="filled"
+        type="number"
       />
-      <br />
-      <ColorButton
-        onClick={() => {
-         if(title.trim()){
-          fetch("http://localhost:3100/mydata", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({price, title}),
-          }).then(()=>{
 
-            navigate("/")
-          })
-         }
-        }}
+      <ColorButton
+        type="submit"
+        onClick={(params) => {}}
         sx={{ mt: "22px" }}
         variant="contained"
       >
-        submit <ChevronRight />
+        Submit <ChevronRight />
       </ColorButton>
     </Box>
   );
 };
 
-export default Creact;
+export default Create;
